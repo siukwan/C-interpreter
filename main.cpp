b99deff90b4eaf6f41c82a98cb32e317d14f6907
siukwanCentOS <pypy39353@qq.com>
2016-01-25 21:22:26 +0800

    增加寄存器和指令集

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@ int *old_text; //dump text段
 int *stack;    //栈
 char*data;     //数据段
 
+//4个虚拟机寄存器
+int *pc, *bp, *s, ax, sycle;
+
+//虚拟机指令集，带有参数的指令在前面
+enum{LEA, IMM, JMP ,CALL ,JZ, JNZ,ENT,ADJ,LEV,LI ,LC,SI ,SC,PUSH,
+		OR,XOR,AND,EQ,NE,LT,GT,LE,GE,SHL,SHR,ADD,SUB,MUL,DIV,MOD,
+		OPEN,READ,CLOS,PRTF,MALC,MSET,MCMP,EXIT};
 int main()
 {
 
@@ -20,5 +27,18 @@ int main()
 		printf("could not malloc(%d) for data segment\n",poolsize);
 		return -1;
 	}
+
+	if(!(stack = malloc(poolsize)))
+	{
+		printf("could not malloc(%d) for stack segment\n",poolsize);
+	}
+	//初始化
+	memset(text,  0, poolsize);
+	memset(data,  0, poolsize);
+	memset(stack, 0, poolsize);
+
+	//初始化寄存器
+	bp = sp = (int*)((int) stack + poolsize);
+	ax = 0;
 
 }
